2fe897226eed956ec20706c4eb1c75a52761c471
kumar <dhirajyadav2055@gmail.com>
2025-06-08 16:58:52 +0530

    testing

---
+++ app/Http/Controllers/Admin/TestingController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TestingController extends Controller
+{
+    Public function testing(Request $request){
+        $data = [];
+        return view('admin.testing', $data);
+    }
+}

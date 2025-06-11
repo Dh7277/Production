87c3c689323470205393bab95c5e5c7dd9ede22c
Dhiraj Yadav <dhirajyadav0584@gmail.com>
2025-06-11 12:50:08 +0530

    Update testing.blade.php

--- resources/views/admin/testing.blade.php
+++ resources/views/admin/testing.blade.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Dhiraj Yadav</title>
 </head>
 <body>
-    <h1>Testing Page!!!</h1>
+    <h1>Shivani Yadav!!!</h1>
+    <h1>Dhiraj Yadav!!!</h1>
+    <h1>Hello Rakhi,--This is tesing page where we will updata the code on the daily basis for completing the project.!!!</h1>
 </body>
 </html>

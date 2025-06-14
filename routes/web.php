d2065ee41480556811afcc3628e36e8adb559a66
kumar <dhirajyadav2055@gmail.com>
2025-06-14 19:43:48 +0530

    abc

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin1\TestingController1;
 use App\Http\Controllers\Admin\AdminLoginController;
-
+use App\Http\Controllers\Admin\BrandController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\FormController;
+use App\Http\Controllers\Admin\HomeController;
+use App\Http\Controllers\Admin\ImageController;
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\Admin\SubCategoryController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -32,45 +38,45 @@ Route::group(['prefix' => 'admin'], function(){
     Route::group(['middleware' => 'admin.guest'],function(){
 
         Route::get('/login', [AdminLoginController::class, 'index'])->name('admin.login'); //Done
-        Route::post('/authenticate', [AdminLoginController::class, 'authenticate'])->name('admin.authenticate');
+        Route::post('/authenticate', [AdminLoginController::class, 'authenticate'])->name('admin.authenticate');//done
 
     });  
 
-    Route::group(['middleware' => 'admin.auth'],function(){
+    // Route::group(['middleware' => 'admin.auth'],function(){
         Route::get('/dashboard', [HomeController::class, 'index'])->name('admin.dashboard');
         Route::get('/logout', [HomeController::class, 'logout'])->name('admin.logout');
 
-    //     Route::get('/form', [FormController::class, 'index'])->name('admin.form');
+        Route::get('/form', [FormController::class, 'index'])->name('admin.form');
 
-    //     //Categories routes
-    //     Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
-    //     Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
-    //     Route::post('/categories/store', [CategoryController::class, 'store'])->name('categories.store');
-    //     Route::get('/categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
-    //     Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
-    //     Route::get('/categories/{category}/destroy', [CategoryController::class, 'destroyPage'])->name('categories.destroyPage');
-    //     Route::put('/category/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
+        //Categories routes
+        Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+        Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
+        Route::post('/categories/store', [CategoryController::class, 'store'])->name('categories.store');
+        Route::get('/categories/{category}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
+        Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
+        Route::get('/categories/{category}/destroy', [CategoryController::class, 'destroyPage'])->name('categories.destroyPage');
+        Route::put('/category/{category}', [CategoryController::class, 'destroy'])->name('categories.destroy');
 
-    //     //SubCategory routes
-    //     Route::get('/sub-categories', [SubCategoryController::class, 'index'])->name('sub-categories.index');
-    //     Route::get('/sub-categories/create', [SubCategoryController::class, 'create'])->name('sub-categories.create');
-    //     Route::post('/sub-categories/store', [SubCategoryController::class, 'store'])->name('sub-categories.store');
-    //     Route::get('/sub-categories/{subCategory}/edit', [SubCategoryController::class, 'edit'])->name('sub-categories.edit');
-    //     Route::put('/sub-categories/{subCategory}', [SubCategoryController::class, 'update'])->name('sub-categories.update');
-    //     Route::delete('/sub-categories/{category}', [SubCategoryController::class, 'destroy'])->name('sub-categories.destroy');
+        //SubCategory routes
+        Route::get('/sub-categories', [SubCategoryController::class, 'index'])->name('sub-categories.index');
+        Route::get('/sub-categories/create', [SubCategoryController::class, 'create'])->name('sub-categories.create');
+        Route::post('/sub-categories/store', [SubCategoryController::class, 'store'])->name('sub-categories.store');
+        Route::get('/sub-categories/{subCategory}/edit', [SubCategoryController::class, 'edit'])->name('sub-categories.edit');
+        Route::put('/sub-categories/{subCategory}', [SubCategoryController::class, 'update'])->name('sub-categories.update');
+        Route::delete('/sub-categories/{category}', [SubCategoryController::class, 'destroy'])->name('sub-categories.destroy');
 
-    //     //Brand routes
-    //     Route::get('/brands', [BrandController::class, 'index'])->name('brands.index');
-    //     Route::get('/brands/create', [BrandController::class, 'create'])->name('brands.create');
-    //     Route::post('/brands/store', [BrandController::class, 'store'])->name('brands.store');
-    //     Route::get('/brands/{brand}/edit', [BrandController::class, 'edit'])->name('brands.edit');
-    //     Route::put('/brands/{brand}', [BrandController::class, 'update'])->name('brands.update');
-    //     Route::delete('/brands/{brand}', [BrandController::class, 'destroy'])->name('brands.destroy');
+        //Brand routes
+        Route::get('/brands', [BrandController::class, 'index'])->name('brands.index');
+        Route::get('/brands/create', [BrandController::class, 'create'])->name('brands.create');
+        Route::post('/brands/store', [BrandController::class, 'store'])->name('brands.store');
+        Route::get('/brands/{brand}/edit', [BrandController::class, 'edit'])->name('brands.edit');
+        Route::put('/brands/{brand}', [BrandController::class, 'update'])->name('brands.update');
+        Route::delete('/brands/{brand}', [BrandController::class, 'destroy'])->name('brands.destroy');
 
-    //     //Product routes
-    //     Route::get('/products', [ProductController::class, 'create'])->name('products.create');
+        //Product routes
+        Route::get('/products', [ProductController::class, 'create'])->name('products.create');
 
-    });
+    // });
  
 });
 
